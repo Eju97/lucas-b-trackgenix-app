0486b35f481864e4d7c1ef1a37fadec2d11e4f02
Luciano Franchino <luciano_franchino@hotmail.com>
2022-10-29 22:04:12 -0300

    TG-32: list task created

--- src/Components/Tasks/index.js
+++ src/Components/Tasks/index.js
@@ -1,9 +1,35 @@
 import styles from './tasks.module.css';
+import { useEffect, useState } from 'react';
 
 function Tasks() {
+  const [tasks, setTask] = useState([]);
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}tasks`)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setTask(response.data);
+      });
+  }, []);
+
   return (
     <section className={styles.container}>
       <h2>Tasks</h2>
+      <table>
+        <tr>
+          <th>Tasks</th>
+          <th>Description</th>
+        </tr>
+        <div>
+          {tasks.map((task) => {
+            return (
+              <tr key={task._id}>
+                <td>{task.description}</td>
+              </tr>
+            );
+          })}
+        </div>
+      </table>
     </section>
   );
 }
