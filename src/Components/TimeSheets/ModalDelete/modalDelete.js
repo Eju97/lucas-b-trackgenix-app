60b7364e0a4671de345dc452615a48537c6a1eb9
Ernesto Bessone <ejubessone@gmail.com>
2022-10-31 12:07:14 -0300

    TG-34: modal component updated and simplified along with updated styles

--- src/Components/TimeSheets/ModalDelete/modalDelete.js
+++ src/Components/TimeSheets/ModalDelete/modalDelete.js
@@ -1,28 +1,24 @@
 import styles from './modalDelete.module.css';
+
 const TimesheetDeleter = (props) => {
-  console.log(props.timesheetId);
-  const handleDelete = () => {
-    props.handleDelete();
-    props.closeModal();
-  };
   if (!props.showModal) {
     return null;
   }
   return (
-    <div className={styles.modalContainer}>
-      <h2 className={styles.modalTitle}>Do you really want to delete this Timesheet?</h2>
-      <div className={styles.buttonContainer}>
-        <button onClick={props.closeModal} className={styles.cancelBtn}>
-          Cancel
-        </button>
-        <button onClick={handleDelete} className={styles.deleteBtn}>
-          Delete
-        </button>
+    <div className={styles.container}>
+      <div className={styles.modalContainer}>
+        <h2 className={styles.modalTitle}>Do you really want to delete this Timesheet?</h2>
+        <div className={styles.buttonContainer}>
+          <button onClick={props.closeModal} className={styles.cancelBtn}>
+            Cancel
+          </button>
+          <button onClick={props.handleDelete} className={styles.deleteBtn}>
+            Delete
+          </button>
+        </div>
       </div>
     </div>
   );
 };
 
 export default TimesheetDeleter;
-// deleteTimesheet
-// <button onClick={() => deleteTimesheet(timesheet._id)}>X</button>
