3affabf72aabd94eab80c20110a387fc6e54235e
Luciano Franchino <luciano_franchino@hotmail.com>
2022-10-31 14:16:01 -0300

    TG-32: modal funcion correct

--- src/Components/Tasks/Modal/modalIndex.jsx
+++ src/Components/Tasks/Modal/modalIndex.jsx
@@ -1,20 +1,27 @@
 import styles from './modal.module.css';
 
 function Modal(props) {
-  if (!props.show) {
+  if (!props.showModal) {
     return null;
   }
-
-  //   const onCloseModal = () => {
-  //     props.onCloseModal();
-  //   };
 
   return (
     <div className={styles.container}>
       <div className={styles.modal}>
         <h3>Delete Task</h3>
         <button onClick={props.closeModal}>Close</button>
-        <button onClick={props.handleDelete}>Delete</button>
+        <button
+          onClick={() => {
+            props.showModal;
+            props.handleDelete(props.listTask._id);
+            props.closeModal;
+            setTimeout(function () {
+              alert(`${props.listTask.description} deleted!`);
+            }, 20);
+          }}
+        >
+          Delete
+        </button>
       </div>
     </div>
   );
