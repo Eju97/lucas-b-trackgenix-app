46c9df0829fa1b8f26b4bed2156e6cde433db86a
Ernesto Bessone <ejubessone@gmail.com>
2022-11-01 18:21:27 -0300

    TG-35: PUT method added

--- src/Components/TimeSheets/Form/timesheetForm.js
+++ src/Components/TimeSheets/Form/timesheetForm.js
@@ -5,6 +5,8 @@ const Form = () => {
   const [projects, setProjects] = useState([]);
   const [tasks, setTasks] = useState([]);
   const [employees, setEmployees] = useState([]);
+  const [formPut, setFormPut] = useState(true);
+  const [errorState, setErrorState] = useState();
   const [timesheetAdded, setTimesheetAdded] = useState({
     description: '',
     date: '',
@@ -13,7 +15,11 @@ const Form = () => {
     project: '',
     employee: ''
   });
-  //  const tasksUrl = `process.env.REAC_APP_API_URL/tasks`;
+
+  const formDate = (date) => {
+    const dateIso = date.substr(0, 10);
+    return dateIso;
+  };
 
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API_URL}/projects`)
@@ -24,78 +30,95 @@ const Form = () => {
   }, []);
 
   useEffect(() => {
-    fetch(`${process.env.REACT_APP_API_URL}/employees`)
-      .then((res) => res.json())
-      .then((response) => {
-        setEmployees(response.data);
-      });
-  }, []);
-
-  useEffect(() => {
     fetch(`${process.env.REACT_APP_API_URL}/tasks`)
       .then((res) => res.json())
       .then((response) => {
         setTasks(response.data);
       });
   }, []);
-  //console.log(projects);
-  //console.log(projects[0].employees[0].employee.name);
-  //useEffect(() => {
-  //  fetch(tasksUrl)
-  //    .then((res) => res.json())
-  //    .then((response) => {
-  //      setTasks(response.data);
-  //    });
-  //}, []);
+
+  useEffect(async () => {
+    const urlForm = new URLSearchParams(window.location.search);
+    const id = urlForm.get('id');
+    if (id) {
+      setFormPut(false);
+      const urlFetch = await fetch(`${process.env.REACT_APP_API_URL}/time-sheets/${id}`, {
+        method: 'GET'
+      });
+      const response = await urlFetch.json();
+
+      setTimesheetAdded({
+        description: response.data.description,
+        date: response.data.date,
+        hours: response.data.hours,
+        project: response.data.project._id,
+        employee: response.data.employee._id,
+        task: response.data.task._id
+      });
+    } else {
+      return null;
+    }
+  }, []);
 
   const onChange = (event) => {
     setTimesheetAdded({ ...timesheetAdded, [event.target.name]: event.target.value });
-  };
-
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    const response = await fetch(`${process.env.REACT_APP_API_URL}/time-sheets`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        description: timesheetAdded.description,
-        date: timesheetAdded.date,
-        hours: timesheetAdded.hours,
-        project: timesheetAdded.project,
-        employee: timesheetAdded.employee,
-        task: timesheetAdded.task
-      })
-    });
-    const data = await response.json();
-    if (!data.error) {
-      window.location.assign('/time-sheets');
+    if (event.target.name === 'project') {
+      const selectedProject = projects.find((project) => project._id === event.target.value);
+      const projectEmployees = selectedProject.employees.map((employee) => employee.employee);
+      setEmployees(projectEmployees);
     }
   };
 
-  //  const projectMap = projects.map((project) => {
-  //    project.employees.map((item) => {
-  //      if (item === project.employees) {
-  //        item.forEach((prop) => {
-  //          return prop;
-  //        });
-  //      }
-  //    });
-  //  });
-  //  console.log(projectMap);
-
-  //   const employeesMap = projectMap.map((employees) => {
-  // return employees;
-  //   });
-  //   console.log(employeesMap);
-
-  //   const otroMap = employeesMap.map((item) => {
-  // return item;
-  //   });
-  //   console.log(otroMap);
+  const onSubmit = async (event) => {
+    if (formPut) {
+      event.preventDefault();
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/time-sheets`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          description: timesheetAdded.description,
+          date: timesheetAdded.date,
+          hours: timesheetAdded.hours,
+          project: timesheetAdded.project,
+          employee: timesheetAdded.employee,
+          task: timesheetAdded.task
+        })
+      });
+      const data = await response.json();
+      if (!data.error) {
+        window.location.assign('/time-sheets');
+      } else {
+        setErrorState(data.message);
+      }
+    } else {
+      const urlForm = new URLSearchParams(window.location.search);
+      const id = urlForm.get('id');
+      event.preventDefault();
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/time-sheets/${id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          description: timesheetAdded.description,
+          date: timesheetAdded.date,
+          hours: timesheetAdded.hours,
+          project: timesheetAdded.project,
+          employee: timesheetAdded.employee,
+          task: timesheetAdded.task
+        })
+      });
+      const data = await response.json();
+      if (!data.error) {
+        window.location.assign('/time-sheets');
+      } else {
+        setErrorState(data.message);
+      }
+    }
+  };
   return (
     <div>
       <form onSubmit={onSubmit} className={styles.container}>
-        <h2>Create a Timesheet</h2>
+        {formPut ? <h2>Create a Timesheet</h2> : <h2>Edit a Timesheet</h2>}
+        <h3>{errorState}</h3>
         <div>
           <div>
             <label htmlFor="description">Description</label>
@@ -113,7 +136,7 @@ const Form = () => {
               name="date"
               type="date"
               required
-              value={timesheetAdded.date}
+              value={formDate(timesheetAdded.date)}
               onChange={onChange}
             />
           </div>
@@ -130,7 +153,7 @@ const Form = () => {
           <div>
             <label htmlFor="project">Project</label>
             <select name="project" required value={timesheetAdded.project} onChange={onChange}>
-              <option value="" selected disabled hidden>
+              <option value="" disabled hidden>
                 Select a project
               </option>
               {projects.map((project) => {
@@ -145,7 +168,7 @@ const Form = () => {
           <div>
             <label htmlFor="employee">Employee</label>
             <select name="employee" required value={timesheetAdded.employee} onChange={onChange}>
-              <option value="" selected disabled hidden>
+              <option value="" disabled hidden>
                 Select an employee
               </option>
               {employees.map((employee) => {
@@ -160,7 +183,7 @@ const Form = () => {
           <div>
             <label htmlFor="task">Task</label>
             <select name="task" required value={timesheetAdded.task} onChange={onChange}>
-              <option value="" selected disabled hidden>
+              <option value="" disabled hidden>
                 Select a task
               </option>
               {tasks.map((task) => {
