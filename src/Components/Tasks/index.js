b22d98ea6985e68451a487ad9fe46d6167565cbc
Ernesto Bessone <ejubessone@gmail.com>
2022-11-04 15:59:24 -0300

    TG-52: Link added to Tasks entity

--- src/Components/Tasks/index.js
+++ src/Components/Tasks/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import TaskList from './TaskList';
 import styles from './tasks.module.css';
 import Modal from './Modal';
+import { Link } from 'react-router-dom';
 
 function Tasks() {
   const [tasks, setTask] = useState([]);
@@ -35,9 +36,9 @@ function Tasks() {
     <div className={styles.container}>
       <TaskList list={tasks} setShowModal={setShowModal} setSelectedId={setSelectedId} />
       <Modal closeModal={closeModal} showModal={showModal} handleDelete={onDeleteTask} />
-      <a href="/tasks/form">
+      <Link to="/tasks/form">
         <img src="../assets/images/add.svg" className={styles.addImg}></img>
-      </a>
+      </Link>
     </div>
   );
 }
