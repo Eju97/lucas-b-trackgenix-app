fd4c626837f4e86263dccbd88f82bf5a06be291b
Ernesto Bessone <ejubessone@gmail.com>
2022-11-11 20:06:13 -0300

    TG-64: Redux, Thunks and Redux-devtools-extension set

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Layout from './Components/Layout';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from './Components/Redux/store';
 import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Layout />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
