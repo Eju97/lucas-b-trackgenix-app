5b918cecf84c87712d1b3786531077fcd8a454e9
Ernesto Bessone <ejubessone@gmail.com>
2022-11-14 11:34:10 -0300

    TG-75: deleteTimesheet thunk added

--- src/redux/timesheets/thunks.js
+++ src/redux/timesheets/thunks.js
@@ -1,4 +1,11 @@
-import { getTimesheetsError, getTimesheetsSuccess, getTimesheetsPending } from './actions';
+import {
+  getTimesheetsError,
+  getTimesheetsSuccess,
+  getTimesheetsPending,
+  deleteTimesheetsError,
+  deleteTimesheetsSuccess,
+  deleteTimesheetsPending
+} from './actions';
 
 export const getTimesheets = () => {
   return (dispatch) => {
@@ -10,6 +17,27 @@ export const getTimesheets = () => {
       })
       .catch((error) => {
         dispatch(getTimesheetsError(error));
+      });
+  };
+};
+
+export const deleteTimesheet = (id) => {
+  return (dispatch) => {
+    dispatch(deleteTimesheetsPending());
+    fetch(`${process.env.REACT_APP_API_URL}/time-sheets/${id}`, {
+      method: 'DELETE'
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        if (response.error) {
+          throw new Error('Could not delete the timesheet');
+        } else {
+          dispatch(deleteTimesheetsSuccess(id));
+        }
+      })
+      .catch((error) => {
+        dispatch(deleteTimesheetsError(error));
       });
   };
 };
