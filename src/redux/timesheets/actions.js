7c3dc1f87728b1a4bdfc4617603de7c49e37a767
Ernesto Bessone <ejubessone@gmail.com>
2022-11-14 01:41:09 -0300

    TG-75: delete actions added

--- src/redux/timesheets/actions.js
+++ src/redux/timesheets/actions.js
@@ -1,8 +1,21 @@
-import { GET_TIMESHEETS_ERROR, GET_TIMESHEETS_PENDING, GET_TIMESHEETS_SUCCESS } from './constants';
+import {
+  GET_TIMESHEETS_ERROR,
+  GET_TIMESHEETS_PENDING,
+  GET_TIMESHEETS_SUCCESS,
+  DELETE_TIMESHEETS_PENDING,
+  DELETE_TIMESHEETS_SUCCESS,
+  DELETE_TIMESHEETS_ERROR
+} from './constants';
 
 export const getTimesheetsPending = () => {
   return {
     type: GET_TIMESHEETS_PENDING
+  };
+};
+
+export const deleteTimesheetsPending = () => {
+  return {
+    type: DELETE_TIMESHEETS_PENDING
   };
 };
 
@@ -13,9 +26,23 @@ export const getTimesheetsSuccess = (payload) => {
   };
 };
 
+export const deleteTimesheetsSuccess = (payload) => {
+  return {
+    type: DELETE_TIMESHEETS_SUCCESS,
+    payload
+  };
+};
+
 export const getTimesheetsError = (error) => {
   return {
     type: GET_TIMESHEETS_ERROR,
+    payload: error
+  };
+};
+
+export const deleteTimesheetsError = (error) => {
+  return {
+    type: DELETE_TIMESHEETS_ERROR,
     payload: error
   };
 };
