8fe3a0490562aeafee072174f4ee03f31beacd8e
Ernesto Bessone <ejubessone@gmail.com>
2022-11-15 01:56:59 -0300

    TG-76: added editTimesheet thunk

--- src/redux/timesheets/thunks.js
+++ src/redux/timesheets/thunks.js
@@ -7,7 +7,10 @@ import {
   deleteTimesheetsPending,
   createTimesheetPending,
   createTimesheetSuccess,
-  createTimesheetError
+  createTimesheetError,
+  editTimesheetPending,
+  editTimesheetSuccess,
+  editTimesheetError
 } from './actions';
 
 export const getTimesheets = () => {
@@ -69,6 +72,35 @@ export const createTimesheet = (newTimesheet) => {
       })
       .catch((error) => {
         dispatch(createTimesheetError(error));
+      });
+  };
+};
+
+export const editTimesheet = (id, timesheet) => {
+  return (dispatch) => {
+    dispatch(editTimesheetPending);
+    fetch(`${process.env.REACT_APP_API_URL}/time-sheets/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        description: timesheet.description,
+        date: timesheet.date,
+        hours: timesheet.hours,
+        project: timesheet.project,
+        employee: timesheet.employee,
+        task: timesheet.task
+      })
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.error) {
+          throw new Error('Could not edit the Timesheet');
+        } else {
+          dispatch(editTimesheetSuccess(response.data));
+        }
+      })
+      .catch((error) => {
+        dispatch(editTimesheetError(error));
       });
   };
 };
