dfb323bf5f824d3ed2d3aaf51e5630f13ee248b9
Luciano Franchino <luciano_franchino@hotmail.com>
2022-11-19 16:46:39 -0300

    create index in shared, import and export all components

---
+++ src/Components/Shared/index.js
@@ -0,0 +1,13 @@
+import Button from './Button';
+import Input from './Input';
+import Modal from './Modal';
+import Select from './Select';
+import Table from './Table';
+
+export default {
+  Button,
+  Input,
+  Modal,
+  Select,
+  Table
+};
