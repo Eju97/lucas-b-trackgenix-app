5fc3a9f2dc5acd60ac9137abe8b2634f46be8cf0
AlejoFGM <alejo.fgm@gmail.com>
2022-11-20 16:36:06 -0300

    TG-100: implement react hook form in task

--- src/Components/Tasks/Form/index.jsx
+++ src/Components/Tasks/Form/index.jsx
@@ -6,13 +6,15 @@ import { useParams, useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { createTask, updateTask, getTask } from '../../../redux/tasks/thunks';
 import { CREATE_TASK_SUCCESS, UPDATE_TASK_SUCCESS } from '../../../redux/tasks/constants';
+import { useForm } from 'react-hook-form';
 
 const TaskForm = () => {
+  const { handleSubmit, register, reset } = useForm({
+    mode: 'onChange'
+  });
+
   const history = useHistory();
   const params = useParams();
-  const [task, setTask] = useState({
-    description: ''
-  });
   const [isEditing, setIsEditing] = useState(false);
   const { isLoading } = useSelector((state) => state.tasks);
   const error = useSelector((state) => state.tasks.error);
@@ -28,19 +30,19 @@ const TaskForm = () => {
     const id = params.id;
     if (id && currentTask) {
       setIsEditing(true);
-      setTask({ description: currentTask.description });
+      reset(currentTask);
     }
   }, [currentTask]);
 
-  const onSubmit = async () => {
+  const onSubmit = async (data) => {
     if (!isEditing) {
-      const response = await dispatch(createTask(task));
+      const response = await dispatch(createTask(data));
       if (response.type === CREATE_TASK_SUCCESS) {
         history.push('/tasks');
       }
     } else {
       const id = params.id;
-      const response = await dispatch(updateTask(id, task));
+      const response = await dispatch(updateTask(id, data));
       if (response.type === UPDATE_TASK_SUCCESS) {
         history.push('/tasks');
       }
@@ -57,22 +59,14 @@ const TaskForm = () => {
       <div>
         <h2>{isEditing ? 'Edit Task' : 'Create New Task'}</h2>
       </div>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className={styles.form}>
-          <Input
-            label="Description"
-            value={task.description}
-            type="text"
-            onChange={(a) => {
-              setTask({
-                description: a.target.value
-              });
-            }}
-          />
+          <Input label="Description" name="description" type="text" register={register} />
         </div>
         <div className={styles.input}>
-          <Button onClick={onSubmit} variant="confirm" name="Submit" />
+          <Button onClick={handleSubmit(onSubmit)} variant="confirm" name="Submit" />
           <Button onClick={() => history.goBack()} variant="cancel" name="Cancel" />
+          <Button onClick={() => reset()} variant="cancel" name="Reset" />
         </div>
       </form>
     </div>
