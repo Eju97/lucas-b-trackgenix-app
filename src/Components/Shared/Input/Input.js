a437e0337422a804ed1e75a588817e52ecb1fa96
Ernesto Bessone <ejubessone@gmail.com>
2022-11-22 16:13:40 -0300

    TG-104: onChange and value props deleted, added some colour to error message on shared input

--- src/Components/Shared/Input/Input.js
+++ src/Components/Shared/Input/Input.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './input.module.css';
 
-const Input = ({ label, id, name, value, placeholder, onChange, type, register, error }) => {
+const Input = ({ label, id, name, placeholder, type, register, error }) => {
   return (
     <div className={styles.container}>
       <label>{label}</label>
@@ -12,8 +12,6 @@ const Input = ({ label, id, name, value, placeholder, onChange, type, register, 
         id={id}
         name={name}
         placeholder={placeholder}
-        value={value}
-        onChange={onChange}
       />
       {error && <p className={styles.warning}>{error}</p>}
     </div>
