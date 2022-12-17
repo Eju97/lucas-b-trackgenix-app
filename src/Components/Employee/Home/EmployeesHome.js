e7dde5a7331a94473fb3c586001103054fa38327
AlejoFGM <alejo.fgm@gmail.com>
2022-12-17 17:42:59 -0300

    TG-140: add employee pm role, view and edit projects

--- src/Components/Employee/Home/EmployeesHome.js
+++ src/Components/Employee/Home/EmployeesHome.js
@@ -4,26 +4,71 @@ import styles from './employ.module.css';
 import Button from 'Components/Shared/Button';
 import { useHistory } from 'react-router-dom';
 import { getTimesheets } from 'redux/timesheets/thunks';
+import { getProjects } from 'redux/projects/thunks';
 import { useDispatch, useSelector } from 'react-redux';
 
 const EmployeeHome = () => {
   const history = useHistory();
   const dispatch = useDispatch();
-  const id = '637aef7b511be17cb78c67b9';
+  const userData = useSelector((state) => state.auth.user);
+  const id = userData._id;
+  const currentProjectPM = useSelector((state) =>
+    state.projects.list.reduce((acc, project) => {
+      const hasEmployee = project.employees.some(
+        (employee) =>
+          employee.length !== 0 &&
+          employee.employee !== null &&
+          employee.employee._id === id &&
+          employee.role === 'PM'
+      );
+      if (hasEmployee) {
+        return [...acc, project];
+      }
+      return acc;
+    }, [])
+  );
   const currentTimesheet = useSelector((state) =>
     state.timesheets.list.filter((timesheet) => timesheet?.employee?._id === id)
+  );
+  const currentTimesheetPM = useSelector((state) =>
+    state.timesheets.list.filter((timesheet) => timesheet?.project._id === currentProjectPM[0]._id)
   );
 
   useEffect(() => {
     dispatch(getTimesheets());
+    dispatch(getProjects());
   }, []);
 
   const dateFormatted = (date) => {
     return new Date(date).toISOString().split('T')[0];
   };
 
+  console.log(currentProjectPM);
   const timeSheetData = () => {
     return currentTimesheet.map((timesheet) => {
+      return {
+        ...timesheet,
+        date: dateFormatted(timesheet.date),
+        task: !timesheet.task ? (
+          <p className={styles.none}>There is no task</p>
+        ) : (
+          timesheet.task.description
+        ),
+        employee: !timesheet.employee ? (
+          <p className={styles.none}>There is no employee</p>
+        ) : (
+          `${timesheet.employee.name} ${timesheet.employee.lastName}`
+        ),
+        project: !timesheet.project ? (
+          <p className={styles.none}>There is no project</p>
+        ) : (
+          timesheet.project.name
+        )
+      };
+    });
+  };
+  const timeSheetDataPM = () => {
+    return currentTimesheetPM.map((timesheet) => {
       return {
         ...timesheet,
         date: dateFormatted(timesheet.date),
@@ -54,6 +99,15 @@ const EmployeeHome = () => {
       <div>
         <Table
           data={timeSheetData()}
+          headers={['description', 'date', 'hours', 'project', 'employee', 'task']}
+        />
+      </div>
+      <div>
+        <h2>My Timesheets PM</h2>
+      </div>
+      <div>
+        <Table
+          data={timeSheetDataPM()}
           headers={['description', 'date', 'hours', 'project', 'employee', 'task']}
         />
       </div>
--- src/Components/Employee/Home/projectTable.js
+++ src/Components/Employee/Home/projectTable.js
@@ -3,15 +3,37 @@ import { getProjects } from 'redux/projects/thunks';
 import { useEffect } from 'react';
 import Table from 'Components/Shared/Table';
 import styles from './employ.module.css';
+import { useHistory } from 'react-router-dom';
 
 const ProjectTable = () => {
   const dispatch = useDispatch();
-  const id = '637aef7b511be17cb78c67b9';
+  const history = useHistory();
+  const userData = useSelector((state) => state.auth.user);
+  const id = userData._id;
   const currentProject = useSelector((state) =>
     state.projects.list.reduce((acc, project) => {
       const hasEmployee = project.employees.some(
         (employee) =>
-          employee.length !== 0 && employee.employee !== null && employee.employee._id === id
+          employee.length !== 0 &&
+          employee.employee !== null &&
+          employee.employee._id === id &&
+          employee.role !== 'PM'
+      );
+      if (hasEmployee) {
+        return [...acc, project];
+      }
+      return acc;
+    }, [])
+  );
+
+  const currentProjectPM = useSelector((state) =>
+    state.projects.list.reduce((acc, project) => {
+      const hasEmployee = project.employees.some(
+        (employee) =>
+          employee.length !== 0 &&
+          employee.employee !== null &&
+          employee.employee._id === id &&
+          employee.role === 'PM'
       );
       if (hasEmployee) {
         return [...acc, project];
@@ -30,6 +52,20 @@ const ProjectTable = () => {
     });
   };
 
+  const projectsDataPM = () => {
+    return currentProjectPM.map((project) => {
+      return {
+        ...project,
+        startDate: dateFormatted(project.startDate),
+        endDate: dateFormatted(project.endDate)
+      };
+    });
+  };
+
+  const onRowClick = (id) => {
+    history.push(`projects/form/${id}`);
+  };
+
   const dateFormatted = (date) => {
     return new Date(date).toISOString().split('T')[0];
   };
@@ -37,6 +73,7 @@ const ProjectTable = () => {
   useEffect(() => {
     dispatch(getProjects());
   }, []);
+  console.log(currentProject);
 
   return (
     <div className={styles.container}>
@@ -44,6 +81,12 @@ const ProjectTable = () => {
       <Table
         data={projectsData()}
         headers={['name', 'clientName', 'description', 'startDate', 'endDate']}
+      />
+      <h2>My projects PM</h2>
+      <Table
+        data={projectsDataPM()}
+        headers={['name', 'clientName', 'description', 'startDate', 'endDate']}
+        onRowClick={onRowClick}
       />
     </div>
   );
