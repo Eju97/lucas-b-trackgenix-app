59bb66b649c1969477218564eee3f5906645f525
Ernesto Bessone <ejubessone@gmail.com>
2022-12-21 20:31:17 -0300

    TG-157: Footer updated

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,48 +1,48 @@
 import styles from './footer.module.css';
 import { withRouter } from 'react-router-dom';
-import { rocket } from 'assets/index';
 
 function Footer() {
   return (
     <footer className={styles.container}>
-      <div className={styles.main}>
-        <div className={styles.appName}>
-          Track<span>GENIX</span>
-        </div>
-        {/* <ul className={styles.rutes}>
-          {props.routes.map((route) => (
-            <li key={route.name}>
-              <Link to={route.path}>{route.name}</Link>
-            </li>
-          ))}
-        </ul> */}
-        <img className={styles.rocketImg} src={rocket} alt="footerImg" />
-      </div>
-      {/* <div className={styles.license}> */}
-      <div className={styles.copyright}>
-        Copyright © {new Date().getFullYear()} TrackGENIX Radium Rocket
-      </div>
       <div>
         <a href={'https://www.facebook.com/radiumrocket'} target={'_blank'} rel="noreferrer">
           <img
             className={styles.socialIcon}
-            src={`${process.env.PUBLIC_URL}/assets/images/facebook.svg`}
+            src={`${process.env.PUBLIC_URL}/assets/images/facebookIcon.svg`}
           />
         </a>
         <a href={'https://twitter.com/radiumrocket'} target={'_blank'} rel="noreferrer">
           <img
             className={styles.socialIcon}
-            src={`${process.env.PUBLIC_URL}/assets/images/twitter.svg`}
+            src={`${process.env.PUBLIC_URL}/assets/images/twitterIcon.svg`}
           />
         </a>
         <a href={'https://www.instagram.com/radium.rocket/'} target={'_blank'} rel="noreferrer">
           <img
             className={styles.socialIcon}
-            src={`${process.env.PUBLIC_URL}/assets/images/instagram.svg`}
+            src={`${process.env.PUBLIC_URL}/assets/images/instagramIcon.svg`}
+          />
+        </a>
+        <a
+          href={'https://www.linkedin.com/company/radium-rocket/'}
+          target={'_blank'}
+          rel="noreferrer"
+        >
+          <img
+            className={styles.socialIcon}
+            src={`${process.env.PUBLIC_URL}/assets/images/linkedinIcon.svg`}
+          />
+        </a>
+        <a href={'https://github.com/radiumrocket/'} target={'_blank'} rel="noreferrer">
+          <img
+            className={styles.socialIcon}
+            src={`${process.env.PUBLIC_URL}/assets/images/githubIcon.svg`}
           />
         </a>
       </div>
-      {/* </div> */}
+      <div className={styles.copyright}>
+        Copyright © {new Date().getFullYear()} TrackGENIX Radium Rocket
+      </div>
     </footer>
   );
 }
